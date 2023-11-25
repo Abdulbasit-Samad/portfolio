17816fdea6ce02c8ba1f6bb3fbdbb27f493eabd5
Abdulbasit-Samad <abdul.basitsamad203@gmail.com>
2023-11-25 17:55:21 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+/**
+ * Application component
+ *
+ * To contain application wide settings, routes, state, etc.
+ */
 
-function App() {
+import React from "react";
+
+import About from "./Components/About";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Home from "./Components/Home";
+import Portfolio from "./Components/Portfolio";
+// import Projects from "./Components/Projects";
+import "./styles.css";
+import Projects from "./Components/Projects";
+import Education from "./Components/Education";
+
+/**
+ * This object represents your information. The project is set so that you
+ * only need to update these here, and values are passed a properties to the
+ * components that need that information.
+ *
+ * Update the values below with your information.
+ *
+ * If you don't have one of the social sites listed, leave it as an empty string.
+ */
+const siteProps = {
+  name: "Abdul Basit",
+  title: "Front End Developer at Zenkoders",
+  email: "k200333@nu.edu.pk",
+  gitHub: "Abdulbasit-Samad",
+  instagram: "basitsamad_/",
+  linkedIn: "abdul-basit-34645a22b/",
+  medium: "",
+  twitter: "",
+  youTube: "",
+};
+
+const primaryColor = "#4E567E";
+const secondaryColor = "#D2F1E4";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="main">
+      <Header />
+      <Home name={siteProps.name} title={siteProps.title} />
+      <About />
+      <Portfolio />
+      <Projects/>
+      <Education/>
+      <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
     </div>
   );
-}
+};
 
 export default App;
